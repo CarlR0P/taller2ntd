183cc3e273751e4cc6db72310c79c5596fcbd34e
LinaCortes24 <linas.cortes24@gmail.com>
2025-03-09 14:19:24 -0500

    Tercer Commit
    Se agrega función para navegar en las secciones

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+function irASeccion(id) {
+    document.querySelectorAll('.seccion-oculta').forEach(seccion => {
+        seccion.style.display = 'none';
+    });
 
+    document.getElementById(id).style.display = 'block';
+}
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
